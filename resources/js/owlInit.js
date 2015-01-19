a4b6bbd47fcd399f150e5b29203250a2df46406a
Rob Russo <rrusso44@gmail.com>
2015-01-19 12:02:44 -0500

    Host DFA RPI
    Temporarily using this to host DFA RPI

---
+++ resources/js/owlInit.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$("#slider").owlCarousel({
+		autoPlay : 4000,
+		navigation: true,
+		slideSpeed: 300,
+		paginationSpeed: 400,
+		navigation: false,
+		singleItem: true
+	});
+})(jQuery);
---
+++ webscraper.php
@@ -0,0 +1,5 @@
+<?php
+	$url = "https://loft.io/dfa-rensselaer/#projects";
+	$output = file_get_contents($url);
+	echo $output;
+?>
