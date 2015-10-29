6dfe4cfd46d637c61fcf7458e8d6f680d1eedc25
Robert Russo <rrusso44@gmail.com>
2015-10-29 16:08:07 -0400

    Early React Work
    Began the processes of adding React. Work not completed

--- res/js/main.js
+++ res/js/main.js
@@ -1,4 +1,27 @@
+var AboutSection = React.createClass({
+  render: function() {
+    return(
+      <div className="col-md-4 aboutme">
+        <h2>{this.props.title}</h2>
+        Not finished converting resume to json yet
+      </div>
+    );
+  }
+});
+
+var About = React.createClass({
+  render: function() {
+    return(
+      <div className="row">
+        <AboutSection title="Projects" />
+        <AboutSection title="Experience" />
+        <AboutSection title="Skills" />
+      </div>
+    );
+  }
+});
+
 React.render(
-  <h1>Hello World</h1>,
-  document.getElementById('header')
+  <About />,
+  document.getElementById('main')
 );
