a2740ac187797254bf7d4ea70c77b497a98e3b97
Robert Russo <rrusso44@gmail.com>
2017-05-02 03:12:18 -0400

    X Informatics Demo

---
+++ demo/index.js
@@ -0,0 +1,8 @@
+// $(window).on('load',function(){
+//   $('#userPreferences').modal('show');
+// });
+
+$('.form-group .btn').click(function(e) {
+  e.preventDefault();
+  $(this).addClass('active');
+});
