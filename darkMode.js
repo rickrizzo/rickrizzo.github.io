e977589fd9c8f2195fe133af9331273108d1562e
Robert Russo <rrusso44@icloud.com>
2017-09-26 17:00:37 -0400

    Fixed dark mode and updated styling

--- darkMode.js
+++ darkMode.js
@@ -1,9 +1,11 @@
 let hour = new Date().getHours();
 let body = document.body.style;
-let a = document.getElementsByTagName('a')[0].style;
+let a = Array.prototype.slice.call(document.getElementsByTagName('a'));
 
 if(hour < 8 || hour > 20) {
   body.backgroundColor = 'black';
   body.color = 'white';
-  a.color = 'orange';
+  a.forEach(function(hyperlink) {
+    hyperlink.style.color = 'orange';
+  });
 }
